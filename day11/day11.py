0ac22d7e52de561216b66051dc1f4ddb9873be2e
Lukasz Romak <lukasz.romak@gmail.com>
2023-12-11 12:46:59 +0100

    day 11 part 2

--- day11/day11.py
+++ day11/day11.py
@@ -8,34 +8,56 @@ def get_pairs(lines, spread):
     matrix = []
     for line in lines:
         if line.count("#") == 0:
-            for i in range(spread):
-                matrix.append(line)
+            matrix.append(spread)
         else:
             matrix.append([*line])
+    
+    matrix = matrix[::-1]
+    x_size = len(matrix[0])
+    y_size = len(matrix)
 
-    matrix = list(zip(*matrix[::-1]))
-
-    galaxies = []
     tmp = []
-    for i, row in enumerate(matrix):        
-        if row.count("#") == 0:
-            for i in range(spread):
-                tmp.append(row)
+    for i in range(x_size):
+        values = []
+        is_empty = True
+        for j in range(y_size):
+            if type(matrix[j]) == int:
+                val = matrix[j]
+            else:
+                val = matrix[j][i]
+            if type(val) != int and val != ".":
+                is_empty = False
+            values.append(val)
+        if is_empty:
+            tmp.append([spread] * y_size)
         else:
-            tmp.append(row)
+            tmp.append(values)
 
     matrix = tmp
-    for i, row in enumerate(matrix):        
-        if row.count("#") > 0:
-            for j, char in enumerate(row):
-                if char == "#":
-                    galaxies.append((i, j))
+    galaxies = []
+    x_distance = 0
+    y_distance = 0
+    for i in range(len(matrix)):
+        if type(matrix[i][0]) == int:
+            x_distance += matrix[i][j]
+            continue
+        y_distance = 0
+        for j in range(len(matrix[i])):
+            if matrix[i][j] == "#":
+                galaxies.append((x_distance, y_distance))
+            if(type(matrix[i][j])) == int:
+                y_distance += matrix[i][j]
+            else:
+                y_distance += 1
+        x_distance += 1
+    
+    return list([(a, b) for idx, a in enumerate(galaxies) for b in galaxies[idx + 1:]])
 
-    pairs = [(a, b) for idx, a in enumerate(galaxies) for b in galaxies[idx + 1:]]
-    return pairs
 
 with open("input.txt") as file:
     lines = [x.strip() for x in file.readlines()]
 
     pairs = get_pairs(lines, 2)
-    print(sum([taxicab_distance(pair[0], pair[1]) for pair in pairs]))
+    print("part1:", sum([taxicab_distance(pair[0], pair[1]) for pair in pairs]))
+    pairs = get_pairs(lines, 1000000)
+    print("part2:", sum([taxicab_distance(pair[0], pair[1]) for pair in pairs]))
